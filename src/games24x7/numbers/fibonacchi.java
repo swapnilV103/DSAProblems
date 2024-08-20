497ade38f67b30e02d19101d6ace459f7b092ffb
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-20 22:40:20 +0530

    logical change

--- src/games24x7/numbers/fibonacchi.java
+++ src/games24x7/numbers/fibonacchi.java
@@ -12,14 +12,14 @@ public class fibonacchi {
 
 	public static void fibo(int num) {
 		// TODO Auto-generated method stub
-		int fibo1= 1;
+		int num2= 1;
 		int num1 = 0;
 		
 		for(int i=0;i<=num;i++) {
 			System.out.print(num1+" ");
-			int next=fibo1+num1;
-			num1=fibo1;
-			fibo1=next;
+			int next=num2+num1;
+			num1=num2;
+			num2=next;
 			
 		}
 		
