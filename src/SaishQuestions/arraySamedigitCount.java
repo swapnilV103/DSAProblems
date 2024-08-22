6400c493d426c0f787b3661842a6b000105f1f9d
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-22 16:33:37 +0530

    hash collection

--- src/SaishQuestions/arraySamedigitCount.java
+++ src/SaishQuestions/arraySamedigitCount.java
@@ -7,17 +7,18 @@ public class arraySamedigitCount {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] array1 = {4,4,5,5,6,6,7,2,4,7,6,4};
+		Integer[] array1 = {4,4,5,5,6,6,7,2,4,7,6,4};
 		String one = Arrays.toString(array1);
 		System.out.print(one);
 		
-		HashSet<Integer> hash = new HashSet<>();
+		HashSet<Integer> hash = new HashSet<Integer>(Arrays.asList(array1));
+		HashSet<Integer> hash1 = new HashSet<Integer>(Arrays.asList(array1));
 		for(int s:array1) {
-			hash.add(s);
+			hash1.add(s);
 		}
 		
 		System.out.print(hash);
-		
+		System.out.print(hash1);
 
 	}
 
