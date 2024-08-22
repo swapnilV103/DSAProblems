9d375ba4098eb8955b544870df765f537c32af9d
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-08-22 15:52:06 +0530

    added hash

---
+++ src/SaishQuestions/arraySamedigitCount.java
@@ -0,0 +1,19 @@
+package SaishQuestions;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class arraySamedigitCount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array1 = {4,4,5,5,6,6,7,2,4,7,6,4};
+		String one = Arrays.toString(array1);
+		System.out.print(one);
+		
+		HashSet<String> hash = new HashSet<>();
+		
+
+	}
+
+}
