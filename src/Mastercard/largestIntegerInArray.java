da46fa96a975c2d2b20a7c647a66785f2323913a
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-09-05 12:41:47 +0530

    Find largest number in array using inbuild sort method, using for each int method and by using a for loop ober the length

---
+++ src/Mastercard/largestIntegerInArray.java
@@ -0,0 +1,35 @@
+package Mastercard;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class largestIntegerInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//int arr[] = new int[5];
+		int[] arr = new int[]{1,2,3,4,1};
+		int[] arr1 = new int[]{1,2,3,4,5,1};
+		Arrays.sort(arr);
+		System.out.println(arr[arr.length-1]);
+		HashSet<Integer> hash = new HashSet<>();
+		
+		for(int a:arr) {
+			System.out.print(a+" ");
+			hash.add(a);
+		}
+		System.out.println();
+		System.out.println(hash);
+		int b=0,temp = 0;
+		for(int a :arr1) {
+			if(a>b) {
+				temp=a;
+			}
+			b=a;
+		}
+		System.out.print(temp);
+		
+
+	}
+
+}
