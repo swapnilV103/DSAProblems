5b370627bd14c9af72f94269de766fd923b941ee
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-09-07 14:57:19 +0530

    logic for second largest element

---
+++ src/Mastercard/secondLargestIntegerinArray.java
@@ -0,0 +1,38 @@
+package Mastercard;
+
+class a{
+	private int a =10;
+	
+	
+	
+	public int  getA() {
+		return a;
+	}
+	
+	public void setA(int a) {
+		this.a=a;
+		
+	}
+	
+}
+
+public class secondLargestIntegerinArray {
+	public static String name = "swap";
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		a b = new a();
+		System.out.println(b.getA());
+		b.setA(20);
+		System.out.print(b.getA());
+		
+		//int c = b.a;
+		//System.out.print(c);
+		
+
+	}
+
+}
