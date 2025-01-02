1922f9ec6962fe4a607be9b617ff320af8983beb
swapnilvidhate <swapnilidawate103@gmail.com>
2025-01-02 23:30:21 +0530

    need to fine tune

--- src/ThoughtWorks/firstDsaClass.java
+++ src/ThoughtWorks/firstDsaClass.java
@@ -2,4 +2,10 @@ package ThoughtWorks;
 
 public class firstDsaClass {
     int a =10;
+    int b=8;
+    public static void main(String[] args) {
+        firstDsaClass obj = new firstDsaClass();
+        System.out.println("Value of a: " + obj.a);
+        System.out.println("Value of b: " + obj.b);
+    }
 }
